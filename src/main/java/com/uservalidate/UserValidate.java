ec8805e403c13311a62e47649c3b865d2dca1ce1
sonvin2916 <sonalchougule07@gmail.com>
2020-07-22 19:47:27 +0530

    [Sonal]Added : User entered a Valid Email

--- src/main/java/com/uservalidate/UserValidate.java
+++ src/main/java/com/uservalidate/UserValidate.java
@@ -5,11 +5,24 @@ import java.util.regex.Pattern;
 public class UserValidate
 {
     private static final String FIRST_NAME="^[A-Z]{1}[a-z]{2,}$";
+    private static final String LAST_NAME="^[A-Z]{1}[a-z]{2,}$";
+    private static final String EMAIL_PATTERN="^[a-zA-Z0-9._%+-]+\\@[a-zA-Z0-9]+\\.[A-Za-z]{2,6}$";
 
     public boolean validateFirstName(String fname)
     {
         Pattern pattern = Pattern.compile(FIRST_NAME);
         return pattern.matcher(fname).matches();
     }
+    public boolean validateLastName(String lname)
+    {
+        Pattern pattern = Pattern.compile(LAST_NAME);
+        return pattern.matcher(lname).matches();
+    }
+    public boolean validateEmail(String email)
+    {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(email).matches();
+    }
+
 
 }
--- src/test/java/com/userregistration/UserValidateTest.java
+++ src/test/java/com/userregistration/UserValidateTest.java
@@ -13,10 +13,39 @@ public class UserValidateTest
         Assert.assertEquals(true,result);
     }
     @Test
-    public void givenFirstName_WithMininum_ThreeCharacters_ShouldReturnFalse()
+    public void givenFirstName_LessThan_ThreeCharacters_ShouldReturnFalse()
     {
         UserValidate validate=new UserValidate();
         boolean result = validate.validateFirstName("So");
         Assert.assertEquals(false,result);
     }
+    @Test
+    public void givenLastName_WithCaps_ShouldReturnTrue()
+    {
+        UserValidate validate= new UserValidate();
+        boolean result = validate.validateLastName("Chougule");
+        Assert.assertEquals(true,result);
+    }
+    @Test
+    public void givenLastName_LessThan_ThreeCharacter_ShiuldReturnFalse()
+    {
+        UserValidate validate=new UserValidate();
+        boolean result = validate.validateFirstName("Ch");
+        Assert.assertEquals(false,result);
+    }
+    @Test
+    public void givenEmail_IfValid_ShouldReturnTrue()
+    {
+        UserValidate validate = new UserValidate();
+        boolean result=validate.validateEmail("sonalchougule07@gmail.com");
+        Assert.assertEquals(true,result);
+    }
+    @Test
+    public void givenEmail_IfInvalid_ShouldReturnFalse()
+    {
+        UserValidate validate = new UserValidate();
+        boolean result = validate.validateEmail("abc.xyz@com");
+        Assert.assertEquals(false,result);
+    }
+    
 }
